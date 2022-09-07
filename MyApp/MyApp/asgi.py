e2996bdde48deb92eb38a6aa93279690f10f8f04
Ali Rahimi <1aliirahimi1@gmail.com>
2022-09-07 10:59:42 +0430

    add the chanell protocol

--- MyApp/MyApp/asgi.py
+++ MyApp/MyApp/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyApp.settings')
-
-application = get_asgi_application()
+                
+                      
+os.environ.setdefault ('DJANGO_SETTINGS_MODULE','mysite.settings')
+application = ProtocolTypeRouter({
+    "http" : get_asgi_application(), 
+}
+)
