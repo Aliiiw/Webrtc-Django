ce6e126f817ab5f6fa51cb0f575d588dff08a5d5
Ali Rahimi <1aliirahimi1@gmail.com>
2022-09-07 12:02:49 +0430

    add js functions

--- MyApp/static/js/main.js
+++ MyApp/static/js/main.js
@@ -1,1 +1,28 @@
 console.log('In main.js!')
+
+
+var inputUsername = document.querySelector('#username');
+var btnJoin = document.querySelector('#btn-join');
+
+var username;
+btnJoin.addEventListener('click', () => {
+    username = inputUsername.value;
+
+    console.log('username: ', username);
+    if(username == '') {
+        return;
+    }
+
+    inputUsername.value = '';
+    inputUsername.disabled = true;
+    inputUsername.style.visibility = 'hidden';
+
+    btnJoin.disabled = true;
+    btnJoin.style.visibility = 'hidden';
+
+    var labelUsername = document.querySelector('#label-username');
+
+    labelUsername.innerHTML = username;
+
+
+});
